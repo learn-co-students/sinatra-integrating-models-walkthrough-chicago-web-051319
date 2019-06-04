336b08ba5ac6ad3e81d19619c95884286ce7d852
Joel Seng <joel29dec@gmail.com>
2019-06-03 22:03:10 -0500

    Done.

--- app.rb
+++ app.rb
@@ -8,7 +8,11 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    @words = @analyzed_text.count_of_words
+    @vowels = @analyzed_text.count_of_vowels 
+    @consonants = @analyzed_text.count_of_consonants
+    @most = @analyzed_text.most_used_letter
     erb :results
   end
 end
